81ec6efb9e6f3d1366d917a021ce7480be84ae55
Michael Kelly <mike@whatsthatweb.com.au>
2017-07-25 09:41:22 +1000

    Small commit for redeploy

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
+ * Modified, seems to work.
  */
 dotenv.config();
 
